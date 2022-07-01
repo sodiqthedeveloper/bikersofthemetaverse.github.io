c6f9936dee5f56345c4eb7e87a5ca0a607c1491b
sodiqthedeveloper <108499124+sodiqthedeveloper@users.noreply.github.com>
2022-07-01 05:27:18 +0400

    Add files via upload

---
+++ bikersofthemetaverse/togglebtn.js
@@ -0,0 +1,6 @@
+const togglebutton= document.getElementsByClassName("btn")[0]
+const thenavlinks=document.getElementsByClassName("thelinks")[0]
+
+togglebutton.addEventListener('click', () =>{
+thenavlinks.classlist.toggle("active")}
+)
